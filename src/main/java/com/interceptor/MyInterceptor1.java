fad170ed9dba68786367ccc3eebb378591057617
JinLu <1124766160@qq.com>
2019-10-27 14:40:47 +0800

    拦截器入门

---
+++ src/main/java/com/interceptor/MyInterceptor1.java
@@ -0,0 +1,56 @@
+package com.interceptor;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author JinLu
+ * @date 2019/10/27
+ */
+public class MyInterceptor1 implements HandlerInterceptor {
+    /**
+     * 在目标方法执行之前 执行
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        System.out.println("preHandle");
+        return true;
+    }
+
+    /**
+     * 在目标方法执行之后 视图对象返回之前执行
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        System.out.println("postHandle");
+    }
+
+    /**
+     * 在整个流程都执行完毕后 执行
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @param ex
+     * @throws Exception
+     */
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        System.out.println("afterCompletion");
+    }
+}
